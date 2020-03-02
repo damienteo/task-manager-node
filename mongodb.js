d95b7ffaa78c7889f2a75cf09239a113b79d402d
damienteo <ksteo89@gmail.com>
2020-03-02 23:25:41 +0800

    Add documents into collections

--- mongodb.js
+++ mongodb.js
@@ -13,6 +13,58 @@ MongoClient.connect(
       return console.log("Error:", error);
     }
 
-    console.log("Connected!");
+    const db = client.db(databaseName);
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "User01",
+    //     age: 30
+    //   },
+    //   (error, result) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "User02",
+    //       age: 31
+    //     },
+    //     {
+    //       name: "User03",
+    //       age: 32
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Clean room",
+          completed: true
+        },
+        {
+          description: "Buy food",
+          completed: false
+        },
+        {
+          description: "Do homework",
+          completed: false
+        }
+      ],
+      (error, result) => {
+        if (error) return console.log("Error:", error);
+
+        return console.log(result.ops);
+      }
+    );
   }
 );
