1d693b78b853b3add42b789bed477cbad755c068
damienteo <ksteo89@gmail.com>
2020-03-03 09:20:37 +0800

    use mongDB methods

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongodb = require("mongodb");
 
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = mongodb;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -16,10 +16,7 @@ MongoClient.connect(
     const db = client.db(databaseName);
 
     // db.collection("users").insertOne(
-    //   {
-    //     name: "User01",
-    //     age: 30
-    //   },
+    //   { _id: id, name: "User04", age: 33 },
     //   (error, result) => {
     //     if (error) return console.log("Error:", error);
 
@@ -45,26 +42,128 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Clean room",
-          completed: true
-        },
-        {
-          description: "Buy food",
-          completed: false
-        },
-        {
-          description: "Do homework",
-          completed: false
-        }
-      ],
-      (error, result) => {
-        if (error) return console.log("Error:", error);
+    //   db.collection("tasks").insertMany(
+    //     [
+    //       {
+    //         description: "Clean room",
+    //         completed: true
+    //       },
+    //       {
+    //         description: "Buy food",
+    //         completed: false
+    //       },
+    //       {
+    //         description: "Do homework",
+    //         completed: false
+    //       }
+    //     ],
+    //     (error, result) => {
+    //       if (error) return console.log("Error:", error);
 
-        return console.log(result.ops);
-      }
-    );
+    //       return console.log(result.ops);
+    //     }
+    //   );
+
+    // db.collection("users").findOne({ name: "User01" }, (error, user) => {
+    //   if (error) return console.log("Error:", error);
+
+    //   return console.log(user);
+    // });
+
+    // db.collection("users")
+    //   .find({ name: "User01" })
+    //   .toArray((error, user) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(user);
+    //   });
+
+    // db.collection("users")
+    //   .find({ name: "User01" })
+    //   .count((error, count) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(count);
+    //   });
+
+    // db.collection("tasks")
+    //   .find()
+    //   .limit(1)
+    //   .sort({ $natural: -1 })
+    //   .toArray((error, task) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(task);
+    //   });
+
+    // db.collection("tasks")
+    //   .find({ completed: false })
+    //   .toArray((error, tasks) => {
+    //     if (error) return console.log("Error:", error);
+
+    //     return console.log(tasks);
+    //   });
+
+    // const updatePromise = db.collection("users").updateOne(
+    //   {
+    //     _id: new ObjectID("5e5d1ee761824e434ae178eb")
+    //   },
+    //   {
+    //     // $set: {
+    //     //   name: "NewName"
+    //     // }
+    //     $inc: {
+    //       age: 100
+    //     }
+    //   }
+    // );
+
+    // updatePromise
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+
+    // db.collection("tasks")
+    //   .updateMany(
+    //     {
+    //       completed: true
+    //     },
+    //     {
+    //       $set: {
+    //         completed: false
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result.modifiedCount);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+
+    // db.collection("users")
+    //   .deleteMany({
+    //     age: 30
+    //   })
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+
+    db.collection("tasks")
+      .deleteOne({
+        description: "Clean room"
+      })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 );
